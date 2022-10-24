199c62d431282d0936f9c6f852a43fb69e4f7309
Aile <Aile@Aile>
2022-10-24 07:03:32 +0700

    Add App code

--- Bus_Booking/src/main/App.java
+++ Bus_Booking/src/main/App.java
@@ -1,10 +1,24 @@
 package main;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("Vui lòng chọn 0: Quản lí, 1: Khách Hàng => ");
+		Scanner sc = new Scanner(System.in);
+		String op = sc.nextLine();
+		
+		while(!op.equals("0") && !op.equals("1")) {
+			System.out.print("Lựa chọn không tồn tại, vui lòng nhập lại: ");
+			op = sc.nextLine();
+		}
+	
+		if(op.equals("0")) {
+			LoginAsManager.main(null);
+		}
+		else{
+			LoginAsCustomer.main(null);
+		}
 	}
-
 }
